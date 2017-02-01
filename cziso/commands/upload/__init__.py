5d6d5550309973f81f0040976469b3cf27d676fe
Shava Smallen <ssmallen@shavas-air.gateway.pace.com>
2017-01-31 20:11:52 -0800

    Added missing option/arg

--- cziso/commands/upload/__init__.py
+++ cziso/commands/upload/__init__.py
@@ -15,11 +15,16 @@ class Command(cziso.commands.Command):
 				"Google Drive ID for folder you want to upload to")
 		],
 		[
+			Opt("description", "New description of file", None),
+			Opt("filename",
+			    "New name for Google drive file (default: existing filename)",
+			    None),
 			Opt(
 				"revision",
 				"Upload the file as a revision of existing Google drive file",
-				"false"),
-			Opt("description", "New description of file", None)
+				"false")
+
+
 		]
 	)
 
@@ -29,18 +34,17 @@ class Command(cziso.commands.Command):
 
 	def run(self, config, args):
 		arg_vals = self.parse_args(args)
-
-		gdrivemodule = None
+		print arg_vals
 		try:
-			gdrivemodule = __import__("cziso.gdrive")
+			__import__("cziso.gdrive")
 		except ImportError as e:
 			cziso.abort("Missing %s" % str(e))
 
-		gdrive = gdrivemodule.GdriveAuth(config)
-
+		gdrive = cziso.gdrive.GdriveAuth(config)
 
 		gdrive.upload(
 			arg_vals["file"],
+			arg_vals["filename"],
 			arg_vals["gdrive_folder"],
 			self.is_arg_true(arg_vals["revision"]),
 			arg_vals["description"])
